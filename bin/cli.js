cd95b0e52390de49021028708a18bf65da9ee4ca
airportyh <airportyh@gmail.com>
2013-05-11 07:46:37 -0400

    Table display for jobs and added device column for browser display.

--- bin/cli.js
+++ bin/cli.js
@@ -163,29 +163,50 @@ function makeBS(){
 }
 
 function displayBrowsers(browsers){
+
   var table = new Table({
-    head: ['Browser', 'OS'],
-    colWidth: [100, 100]
+    head: ['Browser', 'Device', 'OS'],
+    colWidth: [100, 100, 100]
   })
   browsers.forEach(function(browser){
     table.push([
       browserDisplay(browser),
+      deviceDisplay(browser),
       osDisplay(browser)
     ])
   })
-  console.log(table.toString())
-
-  function browserDisplay(browser){
-    var ret = browser.browser
-    if (browser.browser_versio){
-      ret += ' (' + browser.browser_version + ')'
-    }
-    return ret
-  }
-
-  function osDisplay(browser){
-    return browser.os + ' (' + browser.os_version + ')'
-  }
+  console.log(table.toString())  
 }
 
+function displayJobs(jobs){
+  var table = new Table({
+    head: ['ID', 'Browser/Device', 'OS', 'Status'],
+    colWidth: [100, 100, 100]
+  })
+  jobs.forEach(function(job){
+    table.push([
+      job.id,
+      job.browser ? browserDisplay(job) : deviceDisplay(job),
+      osDisplay(job),
+      job.status
+    ])
+  })
+  console.log(table.toString())
+}
+
+function browserDisplay(browser){
+  var ret = browser.browser
+  if (browser.browser_version){
+    ret += ' (' + browser.browser_version + ')'
+  }
+  return ret
+}
+
+function osDisplay(browser){
+  return browser.os + ' (' + browser.os_version + ')'
+}
+
+function deviceDisplay(browser){
+  return browser.device || 'NA'
+}
 
